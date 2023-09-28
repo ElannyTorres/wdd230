9d52e6a6d11ec7f5949a2014c25c50663e1a9a6c
ElannyTorres <torres.elanny@gmail.com>
2023-09-28 00:10:02 -0500

    add: Home Page Enhancement

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+const mainNav = document.querySelector('.navigation');
+const hamBtn = document.querySelector('#menu');
+
+hamBtn.addEventListener('click', () => {
+    mainNav.classList.toggle('show');
+    hamBtn.classList.toggle('show');
+})
+
+const modeBtn = document.querySelector('#mode');
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+const nameTitle = document.querySelector('#name');
+const main = document.querySelector('main');
+
+modeBtn.addEventListener('click', () => {
+    if (modeBtn.textContent.includes("DarkMode")) {
+		body.style.background = "#0c1915";
+		body.style.color = "whitesmoke";
+		nameTitle.style.color = "whitesmoke";
+		main.style.background = "#0c1915";
+		main.style.color = "whitesmoke";
+		modeBtn.textContent = "LightMode";
+	} else {
+		body.style.background = "#eee";
+		nameTitle.style.color = "#0c1915";
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeBtn.textContent = "DarkMode";
+	}
+})
