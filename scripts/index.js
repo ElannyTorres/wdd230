7fb4c3796d2bc7b00b3163564d1c4f6fbed26071
ElannyTorres <torres.elanny@gmail.com>
2023-09-28 00:18:50 -0500

    fix: darkmode

--- scripts/index.js
+++ scripts/index.js
@@ -11,12 +11,14 @@ const body = document.querySelector('body');
 const header = document.querySelector('header');
 const nameTitle = document.querySelector('#name');
 const main = document.querySelector('main');
+const navigation = document.querySelector('.navigation-large');
 
 modeBtn.addEventListener('click', () => {
     if (modeBtn.textContent.includes("DarkMode")) {
 		body.style.background = "#0c1915";
 		body.style.color = "whitesmoke";
 		nameTitle.style.color = "whitesmoke";
+		navigation.style.color = "whitesmoke";
 		main.style.background = "#0c1915";
 		main.style.color = "whitesmoke";
 		modeBtn.textContent = "LightMode";
