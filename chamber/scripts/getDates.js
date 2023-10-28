f6b75993b19e6021c9cac40bb9a5680b0cf775fe
ElannyTorres <torres.elanny@gmail.com>
2023-10-28 18:11:36 -0500

    ADD: weather api

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -47,9 +47,56 @@ function setTimestamp() {
   const timestampField = document.getElementById("timestamp");
   const currentTimestamp = new Date().toISOString();
 
-  timestampField.value = currentTimestamp;
-  console.log(timestampField.value)
+  if (timestampField ) {timestampField.value = currentTimestamp;
+  console.log(timestampField.value)}
 }
 
 //! Call the setTimestamp function when the form is loaded
 document.addEventListener("DOMContentLoaded", setTimestamp);
+
+//* API Weather
+const lat = -13.16;
+const lon = -72.54;
+const units= 'imperial';
+const apiKey= '610ea6e074b908b16358606b62eb5b25';
+const URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
+
+const getWeather = async () => {
+  try {
+    const response = await fetch(URL);
+    if (response.ok) {
+      const data = await response.json();
+      // Filtra los datos para obtener solo los pronósticos al mediodía de los próximos tres días
+      const middayForecasts = data.list.filter(item => {
+        return item.dt_txt.includes('12:00:00');
+      });
+      displayWeather(middayForecasts)
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const displayWeather = (list) => {
+  const weatherList = document.querySelector('#weatherList');
+  if (weatherList) {
+    console.log(list)
+    list.forEach(weather => {
+      let li = document.createElement('li');
+      let date = document.createElement('strong');
+      let temp = document.createElement('p');
+      let icon = document.createElement('img');
+      date.innerHTML = new Date(weather['dt_txt']).toDateString();
+      temp.innerHTML = weather.main.temp + '°F';
+      const iconsrc = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
+      icon.setAttribute('src', iconsrc);
+      icon.setAttribute('alt', 'weather-icon');
+      li.appendChild(date);
+      li.appendChild(temp);
+      li.appendChild(icon);
+      weatherList.appendChild(li)
+    });
+  }
+}
+
+getWeather()
